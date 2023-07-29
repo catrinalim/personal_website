cbce83f262aec7ef5b42b90867506a9a2d52ab43
catrinalim <catrinalim@ufl.edu>
2023-07-29 15:29:40 -0600

    project cards flips with click of mouse

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
   
+  document.addEventListener('DOMContentLoaded', function () {
+    const flipperElements = document.querySelectorAll('.flip-card .flip-card-inner');
+  
+    function flipCardOnClick(event) {
+      const flipContainer = event.currentTarget.closest('.flip-card');
+      const isFlipped = flipContainer.classList.contains('hover');
+  
+      if (isFlipped) {
+        flipContainer.classList.remove('hover');
+        event.currentTarget.style.transform = 'rotateY(0)';
+      } else {
+        flipContainer.classList.add('hover');
+        event.currentTarget.style.transform = 'rotateY(180deg)';
+      }
+    }
+  
+    flipperElements.forEach((flipper) => {
+      flipper.addEventListener('click', flipCardOnClick);
+    });
+  });
+  
